4c70293b371218e457e2e8c3f4645e0b708a841a
iliasReddad <79853388+iliasReddad@users.noreply.github.com>
2022-10-26 10:02:52 +0100

    all commit

---
+++ src/app/app-routing.module copy.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+
+
+// no layouts views
+import { IndexComponent } from "./views/index/index.component";
+import { LandingComponent } from "./views/landing/landing.component";
+import { ProfileComponent } from "./views/profile/profile.component";
+
+const routes: Routes = [
+  // admin views
+ 
+  // no layout views
+  { path: "profile", component: ProfileComponent },
+  { path: "landing", component: LandingComponent },
+  { path: "", component: IndexComponent },
+  { path: "**", redirectTo: "", pathMatch: "full" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/views/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import 'flowbite';
+
+@Component({
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  
+})
+export class LandingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
