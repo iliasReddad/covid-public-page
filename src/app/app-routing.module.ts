559fb5f051d0fe4a2f602da919e5c5d32eb157e7
HamZa2108 <zine.hamza2108@gmail.com>
2022-10-26 21:50:11 +0100

    First phase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,8 +26,7 @@ const routes: Routes = [
   },
   // no layout views
   { path: "profile", component: ProfileComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "", component: IndexComponent },
+  { path: "", component: LandingComponent },
   { path: "**", redirectTo: "", pathMatch: "full" },
 ];
 
