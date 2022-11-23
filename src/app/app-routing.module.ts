c37b64956a79ef271b75135e45405bc739953b77
HamZa2108 <zine.hamza2108@gmail.com>
2022-11-23 18:52:03 +0100

    account creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,11 +27,11 @@ const routes: Routes = [
     path: "admin",
     component: AdminComponent,
     children: [
-      { path: "dashboard", component: DashboardComponent , canActivate:[AuthGuard]},
-      { path: "USER_MANAGEMENT", component: SettingsComponent, canActivate:[AuthGuard]},
-      { path: "tables", component: TablesComponent, canActivate:[AuthGuard]},
-      { path: "Sentimentstables", component: SentTablesComponentComponent, canActivate:[AuthGuard]},
-      { path: "SentimentstStates", component: SentimentalStatsComponent, canActivate:[AuthGuard]},
+      { path: "dashboard", component: DashboardComponent},
+      { path: "USER_MANAGEMENT", component: SettingsComponent},
+      { path: "tables", component: TablesComponent},
+      { path: "Sentimentstables", component: SentTablesComponentComponent},
+      { path: "SentimentstStates", component: SentimentalStatsComponent},
 
       
       { path: "maps", component: MapsComponent , canActivate:[AuthGuard]},
