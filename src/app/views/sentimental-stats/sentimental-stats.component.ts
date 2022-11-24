d56b7d9bfc6c792369fdf9c3a43e32fc143d5f5e
iliasReddad <79853388+iliasReddad@users.noreply.github.com>
2022-11-24 19:11:05 +0100

    fix cards

--- src/app/views/sentimental-stats/sentimental-stats.component.ts
+++ src/app/views/sentimental-stats/sentimental-stats.component.ts
@@ -10,11 +10,14 @@ export class SentimentalStatsComponent implements OnInit {
 
   data: any[] = [];
   Comments: any;
+  length:number = 0;
   constructor(private service:AuthService) { }
 
   ngOnInit(): void {
     this.service.getBackEndData().subscribe((response:any)=>{
       this.Comments = response;
+      this.length= this.Comments.length;
+      
     });
 
 
@@ -22,6 +25,69 @@ export class SentimentalStatsComponent implements OnInit {
       this.data = response;
     
     });
+  }
+
+
+
+  getColor(emotion: string){
+    let color: string = '';
+    switch (emotion) {
+      case 'Anger':
+        color = 'bg-red-200';
+        break;
+      case 'Approval':
+        color = 'bg-green-200';
+        break;
+      case 'Optimism':
+        color = 'bg-blue-200';
+        break;
+      case 'Fear':
+        color = 'bg-orange-200';
+        break;
+      case 'Mistrust':
+        color = 'bg-blueGrey-200';
+        break;
+      case 'Sadness':
+        color = 'bg-grey-200';
+        break;
+      default:
+        color = 'grey';
+        break;
+    }
+
+    return color;
+  
+  }
+
+  getIcon(emotion: string){
+    console.log(emotion);
+    let color: string = '';
+    switch (emotion) {
+      case 'Anger':
+        color = 'fa-solid fa-face-angry';
+        break;
+      case 'Approval':
+        color = 'fa-solid fa-thumbs-up';
+        break;
+      case 'Optimism':
+        color = 'fa-solid fa-face-smile';
+        break;
+      case 'Fear':
+        color = 'fa-solid fa-face-sad-cry';
+        break;
+      case 'Mistrust':
+        color = 'fa-solid fa-question';
+        break;
+      case 'Sadness':
+        color = 'fa-solid fa-face-frown';
+        break;
+      default:
+        color = 'fa-solid fa-face-smile';
+        break;
+    }
+
+    return color;
+  
   }
 
 
