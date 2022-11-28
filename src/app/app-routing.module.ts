33b1c759a146e615a76235ac4de56d3367272a23
iliasReddad <79853388+iliasReddad@users.noreply.github.com>
2022-11-28 21:45:53 +0100

    add VISITOR PAGE MANAGEMENT COMPONENT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,9 @@
+import { ContactSMComponent } from './views/admin/contact-sm/contact-sm.component';
+import { SentimentSMComponent } from './views/admin/sentiment-sm/sentiment-sm.component';
+import { TopicSMComponent } from './views/admin/topic-sm/topic-sm.component';
+import { ServiceSMComponent } from './views/admin/service-sm/service-sm.component';
+import { AboutSMComponent } from './views/admin/about-sm/about-sm.component';
+import { HomeSMComponent } from './views/admin/home-sm/home-sm.component';
 import { TopicStatsComponent } from './views/admin/topic-stats/topic-stats.component';
 import { TopicListComponent } from './views/admin/topic-list/topic-list.component';
 import { SentimentalStatsComponent } from './views/admin/sentimental-stats/sentimental-stats.component';
@@ -27,33 +33,40 @@ import { TopicChartComponent } from './views/admin/topic-chart/topic-chart.compo
 import { SentChartComponent } from './views/admin/sent-chart/sent-chart.component';
 
 const routes: Routes = [
- 
+
   {
     path: "admin",
     component: AdminComponent,
     children: [
-      { path: "dashboard", component: DashboardComponent},
+      { path: "dashboard", component: DashboardComponent },
 
       //USERS
       { path: "USER_MANAGEMENT", component: SettingsComponent },
       { path: "tables", component: TablesComponent },
 
       //SENTIMENT ANALYSIS
-      { path: "Sentimentstables", component: SentTablesComponentComponent},
+      { path: "Sentimentstables", component: SentTablesComponentComponent },
       { path: "SentimentstStates", component: SentimentalStatsComponent },
       { path: "SentimentstChart", component: SentChartComponent },
 
       //TOPIC ANALYSIS
-      { path: "Topictables", component: TopicListComponent},
+      { path: "Topictables", component: TopicListComponent },
       { path: "TopicStates", component: TopicStatsComponent },
       { path: "TopicChart", component: TopicChartComponent },
 
       //VISITOR PAGE MANAGEMENT
+      { path: "HomeSM", component: HomeSMComponent },
+      { path: "AboutSM", component: AboutSMComponent },
+      { path: "ServiceSM", component: ServiceSMComponent },
+      { path: "TopicSM", component: TopicSMComponent },
+      { path: "SentimentSM", component: SentimentSMComponent },
+      { path: 'ContactSM', component: ContactSMComponent },
+
 
 
       { path: "dataAnnotation", component: DataAnnotComponent },
 
-      { path: "maps", component: MapsComponent , canActivate:[AuthGuard]},
+      { path: "maps", component: MapsComponent, canActivate: [AuthGuard] },
       { path: "", redirectTo: "dashboard", pathMatch: "full" },
     ],
   },
@@ -79,4 +92,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
